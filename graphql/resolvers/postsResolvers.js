9b68b80b8e5b6a218d03f0d81c34de4c4d274fe3
Jack M <jackm550@googlemail.com>
2021-08-11 17:10:08 +0100

    Likes implemented!

--- graphql/resolvers/postsResolvers.js
+++ graphql/resolvers/postsResolvers.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server")
+const { AuthenticationError, UserInputError } = require("apollo-server")
 const Post = require("../../models/Post")
 const checkAuth = require("../../util/check-auth")
 
@@ -65,6 +65,32 @@ module.exports = {
         }
       } catch (err) {
         throw new Error(err)
+      }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context)
+
+      //find post
+      const post = await Post.findById(postId)
+
+      //if it exists
+      if (post) {
+        //see if user has already liked the post
+        if (post.likes.find((like) => like.username === username)) {
+          //post already liked, remove like
+          post.likes = post.likes.filter((like) => like.username !== username)
+        } else {
+          //not liked, add like
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          })
+        }
+        //save to db
+        await post.save()
+        return post
+      } else {
+        throw new UserInputError("Post not found!")
       }
     },
   },
